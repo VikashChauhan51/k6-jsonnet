83af60a11dacad977a7fb35119407ba9968232f5
Vikash Chauhan <vikashchauhan51@gmail.com>
2024-03-03 13:12:30 +0530

    added example (#3)
    * added example
    
    * temp fix build
    
    * updated doc

--- examples/main.js
+++ examples/main.js
@@ -1,8 +1,3 @@
-# k6-jsonnet
-k6 jsonnet extension
-
-
-```js
 import jsonnet from 'k6/x/jsonnet';
 
 export default function () {
@@ -10,17 +5,3 @@ export default function () {
   const jsonStr = jsonnet.evaluateJsonnet(jsonnetTemplate);
   console.log(jsonStr);
 }
-
-```
-## Build it
-
-```cmd
-xk6 build --with github.com/VikashChauhan51/k6-jsonnet@v0.1.0
-```
-
-## Run sample script
-
-```cmd
-./k6 run examples/main.js
-
-```
