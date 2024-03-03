528a9d50ed3cc518008f9f4ca76d9602435186b2
Vikash Chauhan <vikashchauhan51@gmail.com>
2024-03-03 12:23:51 +0530

    restructure the package

--- jsonnet.go
+++ jsonnet.go
@@ -2,6 +2,7 @@ package jsonnet
 
 import (
 	gj "github.com/google/go-jsonnet"
+	"go.k6.io/k6/js/modules"
 )
 
 type Extension struct{}
@@ -17,4 +18,9 @@ func (e *Extension) EvaluateJsonnet(jsonnetTemplate string) (string, error) {
 		return "", err
 	}
 	return jsonStr, nil
+}
+
+// Register the extensions on module initialization.
+func init() {
+	modules.Register("k6/x/jsonnet", New())
 }
