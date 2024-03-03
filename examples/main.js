3a532999257f21fe570a573784fd7abb5908cc5f
Vikash Chauhan <vikashchauhan51@gmail.com>
2024-03-03 13:38:20 +0530

    updated example with proper jsonnet template

--- examples/main.js
+++ examples/main.js
@@ -1,7 +1,13 @@
 import jsonnet from 'k6/x/jsonnet';
 
 export default function () {
-  const jsonnetTemplate = '{ "hello": "world" }';
+  const jsonnetTemplate = `{
+    person1: {
+      name: "Alice",
+      welcome: "Hello " + self.name + "!",
+    },
+    person2: self.person1 { name: "Bob" },
+  }`;
   const jsonStr = jsonnet.evaluateJsonnet(jsonnetTemplate);
   console.log(jsonStr);
 }
